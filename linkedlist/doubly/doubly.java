e0711d30e16c5b4f693230c5fef5a2a14472c822
Arwin <ayubanarwin09@gmail.com>
2025-02-19 17:54:22 +0800

    doubly folder and files

---
+++ linkedlist/doubly/doubly.java
@@ -0,0 +1,54 @@
+package repo.linkedlist.doubly;
+
+class Node {
+    int data;
+    Node next, prev;
+
+    public Node(int data) {
+        this.data = data;
+        this.next = null;
+        this.prev = null;
+    }
+}
+
+public class doubly {
+    private Node head;
+
+    public doubly() {
+        this.head = null;
+    }
+
+    public void insert(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node temp = head;
+            while (temp.next != null) {
+                temp = temp.next;
+            }
+            temp.next = newNode;
+            newNode.prev = temp;
+        }
+    }
+
+    public void display() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " <-> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+        doubly list = new doubly();
+        list.insert(10);
+        list.insert(20);
+        list.insert(30);
+        list.insert(40);
+
+        System.out.println("Doubly Linked List:");
+        list.display();
+    }
+}
